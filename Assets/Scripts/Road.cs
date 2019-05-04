1ef7098b96ec61e9797bd48cca9ca2496b6cd440
Valefor <emeline.berenguier@gmail.com>
2019-05-04 14:39:39 +0200

    [ADD] Project

---
+++ Assets/Scripts/Road.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    [SerializeField] Transform _start;
+    [SerializeField] Transform _end;
+
+    [SerializeField] Vector3 _startPoint;
+    [SerializeField] Vector3 _endPoint;
+
+    [SerializeField] float ROAD_SIZE = 5;
+
+    List<RoadSegment> _roadsArray = new List<RoadSegment>();
+    [SerializeField] GameObject _roadInstance;
+
+    Vector3 newPoint;
+
+    private void Start()
+    {
+        _startPoint = _start.position;
+        _endPoint = _end.position;
+    }
+    void CreateRoad()
+    {
+        GameObject lRoad = Instantiate(_roadInstance);
+        lRoad.GetComponent<RoadSegment>().DrawRoad(_startPoint, _endPoint);
+
+        _startPoint = lRoad.GetComponent<RoadSegment>().intersectionPoint;
+        _endPoint = lRoad.GetComponent<RoadSegment>().newPoint;
+
+        _roadsArray.Add(lRoad.GetComponent<RoadSegment>());
+    }
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(10,70,200,40), "Draw Road"))
+        {
+            if (_roadsArray.Count != 0) ResetRoads();
+
+            CreateRoad();
+        }
+    }
+
+    private void ResetRoads()
+    {
+        for(int i = 0; i < _roadsArray.Count; i++)
+        {
+            Destroy(_roadsArray[i].gameObject);
+        }
+
+        _startPoint = _start.position;
+        _endPoint = _end.position;
+
+        _roadsArray.Clear();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(_startPoint, 0.1f);
+        Gizmos.DrawSphere(_endPoint, 0.1f);
+    }
+}
---
+++ Assets/Scripts/RoadSegment.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RoadSegment : MonoBehaviour
+{
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+    public Vector3 intersectionPoint;
+    public Vector3 newPoint;
+    public float roadLenght = 5;
+    Vector3 roadVector;
+
+    LineRenderer lr;
+
+    public void DrawRoad(Vector3 pStartPoint, Vector3 pEndPoint)
+    {
+        lr = GetComponent<LineRenderer>();
+
+        startPoint = pStartPoint;
+        endPoint = pEndPoint;
+
+        roadVector = endPoint - startPoint;
+
+        lr.SetPosition(0, startPoint);
+        lr.SetPosition(1, endPoint);
+
+        intersectionPoint = GetCenterRoad(roadVector);
+        newPoint = GetNewPoint();
+    }
+
+    Vector3 GetNewPoint()
+    {
+        Vector3 lOrthogonalVector = GetOrthogonalVector(roadVector);
+        int lRandomDirection = GetRandomDirection();
+
+        Vector3 lNewPoint = intersectionPoint + (lOrthogonalVector * 5 * lRandomDirection);
+
+        return lNewPoint;
+    }
+
+    Vector3 GetCenterRoad(Vector3 pRoadVector)
+    {
+        Vector3 lCenterPoint = (startPoint + endPoint) / 2;
+        int lRandomDirection = GetRandomDirection();
+
+        lCenterPoint += pRoadVector.normalized * 0.2f * lRandomDirection;
+
+        return lCenterPoint;
+    }
+
+    Vector3 GetOrthogonalVector(Vector3 pRoadVector)
+    {
+        Vector3 lOrthogonalVector = Vector3.Cross(pRoadVector, Vector3.up);
+        lOrthogonalVector.Normalize();
+
+        return lOrthogonalVector;
+    }
+
+    int GetRandomDirection()
+    {
+        int lDirection = Random.Range(-1, 1);
+        if (lDirection == 0) lDirection = 1;
+
+        return lDirection;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(intersectionPoint, 0.1f);
+        Gizmos.DrawSphere(newPoint, 0.1f);
+        Gizmos.DrawLine(intersectionPoint, newPoint);
+    }
+}
